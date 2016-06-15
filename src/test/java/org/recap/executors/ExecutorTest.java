59c8b5830bc560f57b3d8f02e406207a40534e32
govindarajank <govindarajankc@gmail.com>
2016-06-15 16:34:40 +0530

    Added the fields that needs to be indexed.

--- src/test/java/org/recap/executors/ExecutorTest.java
+++ src/test/java/org/recap/executors/ExecutorTest.java
@@ -38,15 +38,67 @@ public class ExecutorTest extends BaseTestCase {
             e.printStackTrace();
         }
 
+        List<String> issnList = new ArrayList<>();
+        List<String>isbnList = new ArrayList<>();
+        List<String> oclcNumberList = new ArrayList<>();
+        issnList.add("0394469756");
+        isbnList.add("0394469755");
+        oclcNumberList.add("00133182");
+        oclcNumberList.add("00440790");
+
         Bib bib1 = new Bib();
         bib1.setId("101");
         bib1.setBarcode("1");
+        bib1.setTitle("Test Bib 1");
+        bib1.setAuthor("Hoepli, Nancy L");
+        bib1.setPublisher("McClelland & Stewart, limited");
+        bib1.setImprint("Toronto, McClelland & Stewart, limited [c1926]");
+        bib1.setIssn(issnList);
+        bib1.setIsbn(isbnList);
+        bib1.setOclcNumber(oclcNumberList);
+        bib1.setPublicationDate("1960");
+        bib1.setMaterialType("Material Type 1");
+        bib1.setNotes("Bibliographical footnotes 1");
+        bib1.setOwningInstitution("PUL");
+        bib1.setSubject("Arab countries Politics and government.");
+        bib1.setPublicationPlace("Paris");
+        bib1.setLccn("71448228");
+
         Bib bib2 = new Bib();
         bib2.setId("102");
         bib2.setBarcode("2");
+        bib2.setTitle("Test Bib 2");
+        bib2.setAuthor("Riddell, William Renwick");
+        bib2.setPublisher("Citadel Press");
+        bib2.setImprint("New York, Citadel Press [1968]");
+        bib2.setIssn(issnList);
+        bib2.setIsbn(isbnList);
+        bib2.setOclcNumber(oclcNumberList);
+        bib2.setPublicationDate("1968");
+        bib2.setMaterialType("Material Type 2");
+        bib2.setNotes("Bibliographical footnotes 2");
+        bib2.setOwningInstitution("PUL");
+        bib2.setSubject("Liberia Social life and customs.");
+        bib2.setPublicationPlace("New York");
+        bib2.setLccn("73596670");
+
         Bib bib3 = new Bib();
         bib3.setId("103");
         bib3.setBarcode("3");
+        bib3.setTitle("Test Bib 3");
+        bib3.setAuthor("Gale, John M");
+        bib3.setPublisher("Random House");
+        bib3.setImprint("New York, Random House [1971, c1970]");
+        bib3.setIssn(issnList);
+        bib3.setIsbn(isbnList);
+        bib3.setOclcNumber(oclcNumberList);
+        bib3.setPublicationDate("1971");
+        bib3.setMaterialType("Material Type 3");
+        bib3.setNotes("Bibliographical footnotes 3");
+        bib3.setOwningInstitution("PUL");
+        bib3.setSubject("Caucasus, South Fiction. Love stories. gsafd");
+        bib3.setPublicationPlace("Paris");
+        bib3.setLccn("68019124");
 
         List<Bib> bibList = asList(bib1, bib2, bib3);
 
