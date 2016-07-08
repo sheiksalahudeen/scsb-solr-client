71c99811db087f24d054e0f8563d072e30334802
Peri Health <info@perihealth.com>
2016-07-08 10:07:35 -0400

    Fixed failing tests.

--- src/main/java/org/recap/repository/jpa/BibliographicDetailsRepository.java
+++ src/main/java/org/recap/repository/jpa/BibliographicDetailsRepository.java
@@ -3,12 +3,13 @@ package org.recap.repository.jpa;
 import org.recap.model.jpa.BibliographicEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by pvsubrah on 6/10/16.
  */
-public interface BibliographicDetailsRepository extends PagingAndSortingRepository<BibliographicEntity, Integer> {
+public interface BibliographicDetailsRepository extends JpaRepository<BibliographicEntity, Integer> {
     Long countByOwningInstitutionId(Integer institutionId);
     Page<BibliographicEntity> findByOwningInstitutionId(Pageable pageable, Integer institutionId);
     BibliographicEntity findByOwningInstitutionIdAndOwningInstitutionBibId(Integer owningInstitutionId, String owningInstitutionBibId);
