babb9bf4411b4a4831dc45672fb141089c5c9fa3
premkumarbalu <prem143india>
2016-08-02 17:04:54 +0530

    Added test cases for coverage

---
+++ src/test/java/org/recap/model/search/SearchRecordsRequestUT.java
@@ -0,0 +1,39 @@
+package org.recap.model.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+/**
+ * Created by premkb on 2/8/16.
+ */
+public class SearchRecordsRequestUT {
+
+    @Test
+    public void testSearchRecordsRequest()throws Exception{
+        SearchRecordsRequest searchRecordsRequest = new SearchRecordsRequest();
+        setSearchRecordsRequest(searchRecordsRequest);
+        assertNotNull(searchRecordsRequest);
+        assertEquals(true,searchRecordsRequest.getSearchResultRows());
+        assertEquals("Available",searchRecordsRequest.getAvailability());
+    }
+
+    private void setSearchRecordsRequest(SearchRecordsRequest searchRecordsRequest){
+        searchRecordsRequest.setShowResults(true);
+        List<SearchResultRow> searchResultRows = new ArrayList<>();
+        SearchResultRow searchResultRow = new SearchResultRow();
+        searchResultRow.setTitle("Title1");
+        searchResultRow.setBibId(1);
+        searchResultRows.add(searchResultRow);
+        searchRecordsRequest.setSearchResultRows(searchResultRows);
+        List<String> availability = new ArrayList<>();
+        availability.add("Available");
+        searchRecordsRequest.setAvailability(availability);
+
+
+    }
+}
