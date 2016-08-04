154050a07895ec8fd48aa8b3f4a637fd31aec25c
rajeshbabuk <rajeshbabu.k@htcindia.com>
2016-08-04 20:05:02 +0530

    Recap-84 : Added call number to marc record view. Sorted item results on chronology and enum and displayed. Toggled plus and minus signs on showing inner table.

--- src/main/resources/static/js/searchRecords.js
+++ src/main/resources/static/js/searchRecords.js
@@ -7,7 +7,21 @@ jQuery(document).ready(function ($) {
 });
 
 function showItems(resultRowIndex) {
+    toggleIcon(resultRowIndex);
     $('#searchResults-' + resultRowIndex).after($("tr.row" + resultRowIndex));
+}
+
+function toggleIcon(resultRowIndex) {
+    var showItemsInputValue = $('#showItemsInput-' + resultRowIndex).val();
+    if (showItemsInputValue == "false") {
+        $('#showItemsInput-' + resultRowIndex).val("true");
+        $('#showItemsIcon-' + resultRowIndex).removeClass("fa-plus-circle");
+        $('#showItemsIcon-' + resultRowIndex).addClass("fa-minus-circle");
+    } else {
+        $('#showItemsInput-' + resultRowIndex).val("false");
+        $('#showItemsIcon-' + resultRowIndex).removeClass("fa-minus-circle");
+        $('#showItemsIcon-' + resultRowIndex).addClass("fa-plus-circle");
+    }
 }
 
 function sortHeader() {
--- src/test/java/org/recap/model/search/SearchItemResultRowUT.java
+++ src/test/java/org/recap/model/search/SearchItemResultRowUT.java
@@ -2,6 +2,10 @@ package org.recap.model.search;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
@@ -32,5 +36,39 @@ public class SearchItemResultRowUT {
         searchItemResultRow.setCollectionGroupDesignation("CG");
         searchItemResultRow.setCustomerCode("NA");
         searchItemResultRow.setUseRestriction("Allowed");
+    }
+
+    @Test
+    public void testSortSearchItemResultRowsOnChronAndEnum() {
+        List<SearchItemResultRow> searchItemResultRows = new ArrayList<>();
+        SearchItemResultRow searchItemResultRow1 = buildSearchItemResultRow("Call Number1", "v. 111-112", "CC 1", "1", "Library Use", "Shared", "Available");
+        SearchItemResultRow searchItemResultRow2 = buildSearchItemResultRow("Call Number2", "v. 116-117", "CC 2", "2", "Library Use", "Shared", "Available");
+        SearchItemResultRow searchItemResultRow3 = buildSearchItemResultRow("Call Number3", "v. 100", "CC 3", "3", "Library Use", "Shared", "Available");
+        SearchItemResultRow searchItemResultRow4 = buildSearchItemResultRow("Call Number4", "v. 113-114", "CC 4", "4", "Library Use", "Shared", "Available");
+        SearchItemResultRow searchItemResultRow5 = buildSearchItemResultRow("Call Number5", "v. 120", "CC 4", "5", "Library Use", "Shared", "Available");
+        searchItemResultRows.add(searchItemResultRow1);
+        searchItemResultRows.add(searchItemResultRow2);
+        searchItemResultRows.add(searchItemResultRow3);
+        searchItemResultRows.add(searchItemResultRow4);
+        searchItemResultRows.add(searchItemResultRow5);
+
+        Collections.sort(searchItemResultRows);
+        assertEquals("v. 100", searchItemResultRows.get(0).getChronologyAndEnum());
+        assertEquals("v. 111-112", searchItemResultRows.get(1).getChronologyAndEnum());
+        assertEquals("v. 113-114", searchItemResultRows.get(2).getChronologyAndEnum());
+        assertEquals("v. 116-117", searchItemResultRows.get(3).getChronologyAndEnum());
+        assertEquals("v. 120", searchItemResultRows.get(4).getChronologyAndEnum());
+    }
+
+    private SearchItemResultRow buildSearchItemResultRow(String callNumber, String chronAndEnum, String customerCode, String barcode, String useRestriction, String collGroupDesg, String availability) {
+        SearchItemResultRow searchItemResultRow = new SearchItemResultRow();
+        searchItemResultRow.setCallNumber(callNumber);
+        searchItemResultRow.setChronologyAndEnum(chronAndEnum);
+        searchItemResultRow.setCustomerCode(customerCode);
+        searchItemResultRow.setBarcode(barcode);
+        searchItemResultRow.setUseRestriction(useRestriction);
+        searchItemResultRow.setCollectionGroupDesignation(collGroupDesg);
+        searchItemResultRow.setAvailability(availability);
+        return searchItemResultRow;
     }
 }
