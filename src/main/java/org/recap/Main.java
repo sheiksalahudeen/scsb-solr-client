59366ee6addb519c461dfa75dfef9712cfea990a
rajeshbabuk <rajeshbabu.k@htcindia.com>
2016-08-25 13:31:47 +0530

    Recap-163 : Modified code to set max request parameter count for the issue in search UI.

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -1,10 +1,14 @@
 package org.recap;
 
+import org.apache.catalina.connector.Connector;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
@@ -21,6 +25,9 @@ public class Main {
 	@Value("${solr.parent.core}")
 	String solrParentCore;
 
+	@Value("${tomcat.maxParameterCount}")
+	Integer tomcatMaxParameterCount;
+
 	@Bean
 	public SolrClient solrAdminClient() {
 		return new HttpSolrClient(solrUrl);
@@ -36,6 +43,18 @@ public class Main {
 	public SolrTemplate solrTemplate(SolrClient solrClient) throws Exception {
 		SolrTemplate solrTemplate = new SolrTemplate(solrClient);
 		return solrTemplate;
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainerFactory() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+			@Override
+			public void customize(Connector connector) {
+				connector.setMaxParameterCount(tomcatMaxParameterCount);
+			}
+		});
+		return factory;
 	}
 
 	public static void main(String[] args) {
