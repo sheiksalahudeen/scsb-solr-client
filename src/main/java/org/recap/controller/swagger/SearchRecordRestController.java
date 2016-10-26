f5432b5155dccd52b46b3ebe08db0d2dd895ccdb
sudhish <ksudhish@gmail.com>
2016-10-26 17:21:17 +0530

    Search Restful With parameter

--- src/main/java/org/recap/controller/swagger/SearchRecordRestController.java
+++ src/main/java/org/recap/controller/swagger/SearchRecordRestController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by sudhish on 13/10/16.
@@ -72,5 +69,53 @@ public class SearchRecordRestController {
             logger.info("search : "+e.getMessage());
         }
         return responseMap;
+    }
+
+
+    @RequestMapping(value="/searchByParam", method = RequestMethod.GET)
+    @ApiOperation(value = "searchParam",notes = "Search Books in ReCAP - Using Method GET, Request data as parameter", nickname = "search", position = 0)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful Search")})
+    public List<SearchResultRow> searchRecordsServiceGet(
+            @RequestParam(name="fieldValue", required = false)  String fieldValue,
+            @ApiParam(name="fieldName",required = false,allowableValues = "Author_search,Title_search,TitleStartsWith,Publisher,PublicationPlace,PublicationDate,Subject,ISBN,ISSN,OCLCNumber,Notes,CallNumber_search,Barcode") @RequestParam(name="fieldName", value = "fieldName" , required = false)  String fieldName,
+            @ApiParam(name="owningInstitutions", value= "Owning Institutions : PUL, CUL, NYPL")@RequestParam(name="owningInstitutions",required = false ) String owningInstitutions,
+            @ApiParam(name="collectionGroupDesignations", value = "collection Designations : Shared,Private,Open") @RequestParam(name="collectionGroupDesignations", value = "collectionGroupDesignations" , required = false)  String collectionGroupDesignations,
+            @ApiParam(name="availability", value = "Availability: Available, NotAvailable") @RequestParam(name="availability", value = "availability" , required = false)  String availability,
+            @ApiParam(name="materialTypes", value = "MaterialTypes: Monograph, Serial, Other") @RequestParam(name="materialTypes", value = "materialTypes" , required = false)  String materialTypes,
+            @ApiParam(name="useRestrictions", value = "Use Restrictions: NoRestrictions, InLibraryUse, SupervisedUse") @RequestParam(name="useRestrictions", value = "useRestrictions" , required = false)  String useRestrictions,
+            @ApiParam(name="pageSize", value = "Page Size in Numers - 10, 20 30...") @RequestParam(name="pageSize", required = false) Integer pageSize
+    ) {
+
+        SearchRecordsRequest searchRecordsRequest = new SearchRecordsRequest();
+        if (fieldValue !=null) {
+            searchRecordsRequest.setFieldValue(fieldValue);
+        }
+        if (fieldName !=null) {
+            searchRecordsRequest.setFieldName(fieldName);
+        }
+        if(owningInstitutions !=null) {
+            searchRecordsRequest.setOwningInstitutions(Arrays.asList(owningInstitutions.split(",")));
+        }else{
+
+        }
+        if(collectionGroupDesignations !=null) {
+            searchRecordsRequest.setCollectionGroupDesignations(Arrays.asList(collectionGroupDesignations.split(",")));
+        }
+        if(availability !=null) {
+            searchRecordsRequest.setAvailability(Arrays.asList(availability.split(",")));
+        }
+        if(materialTypes !=null) {
+            searchRecordsRequest.setMaterialTypes(Arrays.asList(materialTypes.split(",")));
+        }
+        if(pageSize !=null) {
+            searchRecordsRequest.setPageSize(pageSize);
+        }
+        List<SearchResultRow> searchResultRows = null;
+        try {
+            searchResultRows = searchRecordsUtil.searchRecords(searchRecordsRequest);
+        } catch (Exception e) {
+            searchResultRows = new ArrayList<>();
+        }
+        return searchResultRows;
     }
 }
