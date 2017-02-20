6be3c1499de53409db5ef847a75ab6dc5c745220
rajeshbabuk <rajeshbabu.k@htcindia.com>
2017-02-20 16:56:35 +0530

    Recap-204 : Modified code for deaccession. Added controller to deaccession items in solr. Added UT.

---
+++ src/test/java/org/recap/controller/DeaccessionSolrControllerUT.java
@@ -0,0 +1,31 @@
+package org.recap.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.recap.model.deAccession.DeAccessionSolrRequest;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by rajeshbabuk on 15/2/17.
+ */
+public class DeaccessionSolrControllerUT extends BaseControllerUT {
+
+    @Test
+    public void deaccessionInSolr() throws Exception {
+        DeAccessionSolrRequest deAccessionSolrRequest = new DeAccessionSolrRequest();
+        ObjectMapper objectMapper = new ObjectMapper();
+        MvcResult mvcResult = this.mockMvc.perform(post("/deaccessionInSolrService/deaccessionInSolr")
+                .headers(getHttpHeaders())
+                .contentType(contentType)
+                .content(objectMapper.writeValueAsString(deAccessionSolrRequest)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String result = mvcResult.getResponse().getContentAsString();
+        assertNotNull(result);
+    }
+}
